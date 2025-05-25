e21f670887eaf404334828fba31be1f1eaa6077c
Praroop Anand <anandpraroop@gmail.com>
2025-05-25 14:06:29 +0530

    2d Array Adv Questions

--- 2darray.cpp
+++ 2darray.cpp
@@ -1,21 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int printSum(int arr[][3],int row,int col){ //Row wise print Biggest sum
+    int maxi = INT_MIN;
+    int rowIndex = -1;
+    
+    for(int i =0; i<row;i++){
+        int sum = 0;
+        for(int j=0;j<col;j++){
+            sum += arr[i][j];
+        }
+        if(sum>maxi){
+            maxi= sum;
+        }
+        rowIndex = i;
+    }
+    cout << "Biggest Sum is " << maxi <<endl;
+    return rowIndex;
+}
+
 int main(){
-    int arr[3][4];
+    int arr[3][3];
 
     for(int i =0; i<3;i++){
-        for(int j=0;j<4;j++){
+        for(int j=0;j<3;j++){
             cin >> arr[i][j];
         }
     }
 
     for(int i =0; i<3;i++){
-        for(int j=0;j<4;j++){
+        for(int j=0;j<3;j++){
             cout << arr[i][j] << " ";
         }
         cout << endl;
     }
+    cout << printSum(arr,3,3) << endl; 
 
 return 0;
 }
